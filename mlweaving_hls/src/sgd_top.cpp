244a6b83fb267b44fd5cbdfabc633dd32be1a152
qqwert0 <619740073@qq.com>
2019-08-19 14:46:02 +0800

    修改内容 1）修改了内部逻辑，增加了两个函数，对计算过程进行优化

--- mlweaving_hls/src/sgd_top.cpp
+++ mlweaving_hls/src/sgd_top.cpp
@@ -1,15 +1,70 @@
 #include "sgd_top.h"
+
+void dot_product(SGD_PARAM_CONFIG param,
+		int64& mem_addr,
+		CacheLine mem_data,
+		hls::stream<CacheLine>&  CacheLineFifo,
+		int32 cur_sample,
+		int32& Q,
+		int32& x)
+{
+
+
+	for(int k = 0;k < 32;k++)
+	{
+#pragma HLS pipeline II=1
+		mem_addr = param.addr_a + (cur_sample<<6) + k;
+		CacheLineFifo.write(mem_data);
+		for(int i = 0;i < BANK; i++)
+		{
+#pragma HLS unroll factor=8
+			for(int n = 0;n<64;n++)
+			{
+				Q[i] = Q[i] + ((k<param.number_of_bits)? mem_data[i*64+n]*(x[cur_sample+n]>>k) : 0);
+			}
+		}
+	}
+}
+
+void gradient(SGD_PARAM_CONFIG param,
+		hls::stream<CacheLine>&  CacheLineFifo,
+		int32& scale,
+		int32& G)
+{
+	CacheLine a_data;
+	for(int k = 0;k < 32;k++)
+	{
+#pragma HLS pipeline II=1
+		CacheLineFifo.read(a_data);
+		for(int i = 0;i < BANK; i++)
+		{
+#pragma HLS unroll factor=8
+			for(int n = 0;n<64;n++)
+			{
+				G[k+n] = G[k+n] + ((k<param.number_of_bits)? a_data[i*64+n]*(scale[i]>>k) : 0);
+			}
+		}
+	}
+}
+
+
 void sgd_top(SGD_PARAM_CONFIG param,int64* mem_addr,CacheLine mem_data)
 {
 #pragma HLS INTERFACE ap_ovld port=mem_addr
 #pragma HLS INTERFACE ap_vld port=param
 #pragma HLS INTERFACE ap_vld port=mem_data
-	int a_dot_x;
-	int scale;
-	int64* x ;
-	int64 Q[BANK] ;
-	int64* G ;
 
+	  static hls::stream<CacheLine>     CacheLineFifo("CacheLineFifo");
+	  #pragma HLS STREAM variable=CacheLineFifo depth=4
+	  #pragma HLS DATA_PACK variable=CacheLineFifo
+
+
+	int a_dot_x;
+	int32* scale;
+	int32* x ;
+	int32 Q[BANK] ;
+	int32* G ;
+	int32 sample_index;
 
 	for(int e = 1;e < param.number_of_epochs; e++){
 		for(int i = 0;i < param.number_of_samples; i = i + param.mini_batch_size)
@@ -21,35 +76,19 @@ void sgd_top(SGD_PARAM_CONFIG param,int64* mem_addr,CacheLine mem_data)
 
 			for(int j = 0;j < param.mini_batch_size;j = j + BANK)
 			{
+				sample_index = i + j;
+				for(int k = 0;k < param.dimension;k = k + 64)
+				{
+					dot_product(param,*mem_addr,mem_data,CacheLineFifo,sample_index,*Q,*x);
+				}
+				*mem_addr = param.addr_b + (sample_index>>1) ;
 				for(int m = 0;m < BANK; m++)
 				{
-					for(int k = 0;k < param.dimension;k = k + 64)
-					{
-						for(int l = 0;l < param.number_of_bits;l++)
-						{
-							*mem_addr = param.addr_a + (i+j)/BANK*param.number_of_bits + k/64*param.number_of_bits + l;
-							for(int n = 0;n<64;n++)
-							{
-								Q[m] = Q[m] + mem_data[m*64+n]*(x[k+n]>>l);  //每个sample计算其h(a)=a(0)*x(0)+a(1)*x(1)+...+a(m)*x(m);m个特征
-							}
-
-						}
-					}
-					*mem_addr = param.addr_b + i + j + m;
-					scale = param.learning_rate * (Q[m] - mem_data);//计算每个参数的rate*(h(a)-b)
-					for(int k = 0;k < param.dimension;k = k + 64)
-					{
-						for(int l = 0;l < param.number_of_bits;l++)
-						{
-							*mem_addr = param.addr_a + (i+j)/BANK*param.number_of_bits + k/64*param.number_of_bits + l;
-							for(int n = 0;n<64;n++)
-							{
-								G[k+n] = G[k+n] + mem_data[m*64+n]*(scale>>l);//计算loss fuction导数J'(x(i))=(h(a)-b)*a(i)
-							}
-
-						}
-					}
-
+					scale[m] =  sample_index[0] ? (param.learning_rate * (Q[m] - mem_data(32*(m+1)-1,32*m))) : (param.learning_rate * (Q[m] - mem_data(32*(m+1)+255,32*m+256))) ;//计算每个参数的rate*(h(a)-b)
+				}
+				for(int k = 0;k < param.dimension;k = k + 64)
+				{
+					gradient(param,CacheLineFifo,*scale,*G);
 				}
 
 			}
